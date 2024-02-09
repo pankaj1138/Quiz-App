edc67619bd8d6b06bc091439b2d2a3ac24e352d4
pankaj1138 <112249552+pankaj1138@users.noreply.github.com>
2024-02-09 16:28:00 +0530

    Add files via upload

---
+++ Assessment/data/common-knowledge.js
@@ -0,0 +1,209 @@
+const quizData = {
+  title: "Quize app Application"
+};
+
+const questions = [
+  {
+    text: "What is the capital of France? ?",
+    type: "mc",
+    answers: [
+      { text: "Berlin", correct: false },
+      { text: "Madrid", correct: false },
+      { text: "Paris", correct: true},
+      { text: "Rome", correct: false }
+    ]
+  },
+  {
+    text: "In which year did World War II end?",
+    type: "mc",
+    answers: [
+      { text: "1943", correct: false },
+      { text: "1945", correct: true },
+      { text: "1947", correct: false},
+      { text: "1950", correct: false }
+    ]
+  },
+  {
+    text: "What is the largest planet in our solar system?",
+    type: "mc",
+    answers: [
+      { text: "Earth", correct: false },
+      { text: "Jupiter", correct: true },
+      { text: "Saturn", correct: false},
+      { text: "Mars", correct: false }
+    ]
+  },
+  {
+    text: "Who wrote Romeo and Juliet ?",
+    type: "mc",
+    answers: [
+      { text: "Charles Dickens", correct: false },
+      { text: "William Shakespeare", correct: true },
+      { text: "Jane Austen", correct: false},
+      { text: "Mark Twain", correct: false }
+    ]
+  },
+  {
+    text: "What is the chemical symbol for gold?",
+    type: "mc",
+    answers: [
+      { text: "Au", correct: true },
+      { text: "Ag", correct: false },
+      { text: "Fe", correct: false},
+      { text: "Hg", correct: false }
+    ]
+  },
+  {
+    text: "Which planet is known as the  Red Planet?",
+    type: "mc",
+    answers: [
+      { text: "Venus", correct: false },
+      { text: "Mars", correct: true },
+      { text: "Jupiter", correct: false},
+      { text: "Saturn", correct: false }
+    ]
+  },
+  {
+    text: "Who painted the Mona Lisa?",
+    type: "mc",
+    answers: [
+      { text: "Vincent van Gogh", correct: false },
+      { text: "Pablo Picasso", correct: false },
+      { text: "Leonardo da Vinci", correct: true},
+      { text: "Michelangelo", correct: false }
+    ]
+  },
+  {
+    text: "What is the capital of Japan??",
+    type: "mc",
+    answers: [
+      { text: "Beijing", correct: true },
+      { text: "Tokyo", correct: false },
+      { text: "Seoul", correct: false},
+      { text: "Bangkok", correct: false }
+    ]
+  },
+  {
+    text: "What is the largest ocean on Earth?",
+    type: "mc",
+    answers: [
+      { text: "Atlantic Ocean", correct: false },
+      { text: " Indian Ocean", correct: false },
+      { text: " Southern Ocean", correct: false},
+      { text: "Pacific Ocean", correct: true }
+    ]
+  },
+  {
+    text: "Who is the author of To Kill a Mockingbird?",
+    type: "mc",
+    answers: [
+      { text: " J.K. Rowling", correct: true },
+      { text: "Harper Lee", correct: false },
+      { text: "George Orwell", correct: false},
+      { text: "Ernest Hemingway", correct: false }
+    ]
+  },
+  {
+    text: "Which element has the chemical symbol O ?",
+    type: "mc",
+    answers: [
+      { text: "Oxygen", correct: true },
+      { text: "Gold", correct: false },
+      { text: "Silver", correct: false},
+      { text: "Carbon", correct: false }
+    ]
+  },
+  {
+    text: "In which year did the Titanic sink?",
+    type: "mc",
+    answers: [
+      { text: "1905", correct: true },
+      { text: "1912", correct: false },
+      { text: "1920", correct: false},
+      { text: "1931", correct: false }
+    ]
+  },
+  {
+    text: "What is the currency of Canada?",
+    type: "mc",
+    answers: [
+      { text: "Euro", correct: false },
+      { text: "Dollar", correct: true },
+      { text: "Peso", correct: false},
+      { text: "Yen", correct: false }
+    ]
+  },
+  {
+    text: "Who is known as the Father of Modern Physics ?",
+    type: "mc",
+    answers: [
+      { text: "Isaac Newton", correct: false },
+      { text: "Albert Einstein", correct: true },
+      { text: "Galileo Galilei", correct: false},
+      { text: "Nikola Tesla", correct: false }
+    ]
+  },
+  {
+    text: "What is the largest mammal on Earth??",
+    type: "mc",
+    answers: [
+      { text: "Elephant", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Giraffe", correct: false},
+      { text: "Gorilla", correct: false }
+    ]
+  },
+  {
+    text: "Which country is known as the Land of the Rising Sun ?",
+    type: "mc",
+    answers: [
+      { text: "China", correct: false },
+      { text: "South Korea", correct: false },
+      { text: "Japan", correct: true},
+      { text: "Vietnam", correct: false }
+    ]
+  },
+  {
+    text: "Who wrote 1984 ?",
+    type: "mc",
+    answers: [
+      { text: "Aldous Huxley", correct: true },
+      { text: " George Orwell", correct: false },
+      { text: "Ray Bradbury", correct: false},
+      { text: "H.G. Wells", correct: false }
+    ]
+  },
+  {
+    text: "What is the largest desert in the world?",
+    type: "mc",
+    answers: [
+      { text: "Sahara Desert", correct: true },
+      { text: "Gobi Desert", correct: false },
+      { text: "Antarctic Desert", correct: false},
+      { text: "Arctic Desert", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place Who painted the Starry Night?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: false },
+      { text: "Kolkata", correct: true },
+      { text: "Mumbai", correct: false }
+    ]
+  },
+  {
+    text: "What is my favourite place ?",
+    type: "mc",
+    answers: [
+      { text: "Delhi", correct: false },
+      { text: "Maligram,pingla", correct: true },
+      { text: "Kolkata", correct: false },
+      { text: "Mumbai", correct: false }
+    ]
+  }
+
+];
+
+module.exports = { quizData, questions };
